094ea1d50c494106453982bffeb303a46110cefd
Oytun Emre Yucel <oytuneyucel@gmail.com>
2021-02-22 14:33:34 +0300

    Initial Commit

---
+++ CCTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CCTest
+//
+//  Created by Oytun Yücel on 2/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
