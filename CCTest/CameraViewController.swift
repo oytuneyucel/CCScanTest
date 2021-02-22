1a488739ca95caea314d3e85c690633ce9a84faa
Oytun Emre Yucel <oytuneyucel@gmail.com>
2021-02-22 15:25:49 +0300

    add card scan

---
+++ CCTest/CameraViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  CCTest
+//
+//  Created by Oytun Yücel on 2/22/21.
+//
+
+import UIKit
+import CardScan
+
+class CameraViewController: UIViewController, ScanDelegate {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    
+    // It's important that this goes in viewWillAppear because the user may deny permission
+    // on the ScanViewController, in which case you'd want to hide the button to avoid
+    // future presses
+        if !ScanViewController.isCompatible() {
+        // Hide your "scan card" button because this device isn't compatible with CardScan
+        }
+    }
+    
+    @IBAction func scanCardButtonPressed() {
+        guard let vc = ScanViewController.createViewController(withDelegate: self) else {
+        print("This device is incompatible with CardScan")
+        return
+    }
+
+        self.present(vc, animated: true)
+    }
+
+    func userDidSkip(_ scanViewController: ScanViewController) {
+        self.dismiss(animated: true)
+    }
+    
+    func userDidCancel(_ scanViewController: ScanViewController) {
+        self.dismiss(animated: true)
+    }
+    
+    func userDidScanCard(_ scanViewController: ScanViewController, creditCard: CreditCard) {
+        
+            let dictParams: NSMutableDictionary? = ["test" : "test",
+                "ccNo" : creditCard.number, // Always use optional values carefully!
+                "month" : creditCard.expiryMonth ?? "",
+                "year" : creditCard.expiryYear ?? "",
+                "cvv" : creditCard.name ?? ""
+            ]
+            
+            print("dict %@" , dictParams);
+
+    // At this point you have the credit card number and optionally the expiry.
+    // You can either tokenize the number or prompt the user for more
+    // information (e.g., CVV) before tokenizing.
+
+        self.dismiss(animated: true)
+    }
+}
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '12.0'
+
+target 'CCTest' do
+  pod 'CardScan', '1.0.4059'
+  use_frameworks!
+
+  # Pods for CCTest
+
+end
